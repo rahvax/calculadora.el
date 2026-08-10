8c9fd75e341af8843d6a0a3631b323058a295c45
rahvax <gabriel@caelestis.one>
2026-08-10 19:13:34 -0300

    First commit

---
+++ calculadora.el
@@ -0,0 +1,10 @@
+;;; calculadora.el --- apenas o primeiro projeto possível em algo -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;; Esse projeto foi criado para que eu pratique Elisp um pouco.
+;;; A ideia é apenas uma calculadora padrão e dinâmica em Elisp.
+
+;;; Code:
+(+ 2 2)
+
+;;; calculadora.el ends here
